aa48672c8242a3164376e3faa716ac8aadf532da
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-10-01 00:54:49 -0700

    removed console logs

--- resources/script.js
+++ resources/script.js
@@ -19,23 +19,23 @@ function calculateBmr() {
     const height = parseFloat(inputHeight.value).toFixed(2);
     const weight = parseFloat(inputWeight.value).toFixed(2);
     const age = inputAge.value;
-    const gender = inputGender.value;
+    // const gender = inputGender.value;
 
     //check to see if height, weight, or age are numbers
     //if not a number return null
     if (isNaN(height) || isNaN(weight) || isNaN(age)) 
     return;
 
-    console.log(gender)
-    console.log(height)
-    console.log(weight)
-    console.log(age)
+    // console.log(gender)
+    // console.log(height)
+    // console.log(weight)
+    // console.log(age)
 
     //for loop to calculate bmr
     if (inputGender.value == "female") {
         const calculatedBmr = (655 + (4.35 * weight) + (4.7 * height) + (4.7 * age));
         displayBmr.innerHTML = calculatedBmr;
-        console.log(inputGender.value)
+        // console.log(inputGender.value)
     }
     else {
         const calculatedBmr = (66 + (6.2 * weight) + (12.7 * height) + (6.76 * age))
